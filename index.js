65cf07c0ba2010291db14acdc70d82a1e34f7bec
RotaruPaulCristian <cristian.rotaru96@yahoo.com>
2022-11-04 00:50:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+function openCity(e, cityName) {
+  let i;
+
+  // Get all elements with class="tabcontent" and hide them
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  e.currentTarget.className += " active";
+}
